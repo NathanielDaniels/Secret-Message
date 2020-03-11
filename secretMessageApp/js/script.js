a5cbd41402e08c32be42f3ba3c343a2277140a57
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-10 20:35:42 -0500

    finished message section. Everything working

--- secretMessageApp/js/script.js
+++ secretMessageApp/js/script.js
@@ -2,6 +2,11 @@ const { hash } = window.location;
 
 const message = atob(hash.replace("#", ""));
 
+if (message) {
+  document.querySelector("#message-form").classList.add("hidden");
+  document.querySelector("#message-show").classList.remove("hidden");
+  document.querySelector("h2").innerHTML = message;
+}
 const input = document.querySelector("#message-input");
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
@@ -19,33 +24,25 @@ document.querySelector("form").addEventListener("submit", e => {
 });
 
 // Decode Button
-const decodeBtn = document.querySelector("#decodeBtn");
-decodeBtn.addEventListener("click", () => {
-  // const newEl = document.createElement("div");
-  // newEl.classList.add("card-panel", "z-depth-4");
-  // newEl.innerHTML = `${input.value}`;
-  // newEl.style =
-  //   "position: absolute;display: flex; justify-content: center; align-items: center; font-size: 2rem; background: white; color: black; padding: 100px 150px";
+// const decodeBtn = document.querySelector("#decodeBtn");
+// decodeBtn.addEventListener("click", () => {
+//   const newEl = document.createElement("div");
+//   newEl.classList.add("card-panel", "z-depth-4");
+//   newEl.innerHTML = `${input.value}`;
+//   newEl.style =
+//     "position: absolute;display: flex; justify-content: center; align-items: center; font-size: 2rem; background: white; color: black; padding: 100px 150px";
+//   closeX = document.createElement("div");
+//   closeX.innerHTML = "X";
+//   closeX.style = "position: absolute; top: 0; right: 15px; cursor: pointer";
+//   closeX.addEventListener("click", () => {
+//     newEl.classList.add("hidden");
+//   });
+//   console.log(closeX);
+//   newEl.append(closeX);
+//   document.body.appendChild(newEl);
+// });
 
-  if (message) {
-    document.querySelector("#message-form").classList.add("hidden");
-    document.querySelector("#message-show").classList.remove("hidden");
-
-    document.querySelector("h1").innerHtml = message;
-  }
-
-  // closeX = document.createElement("div");
-  // closeX.innerHTML = "X";
-  // closeX.style = "position: absolute; top: 0; right: 15px; cursor: pointer";
-  // closeX.addEventListener("click", () => {
-  //   newEl.classList.add("hidden");
-  // });
-  // console.log(closeX);
-  // newEl.append(closeX);
-
-  document.body.appendChild(newEl);
-});
-
+//================================================
 // const change1 = btoa("Nathan");
 // console.log(change1);
 
