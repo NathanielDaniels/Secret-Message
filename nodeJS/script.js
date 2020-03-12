b714b587fe7d22b9ac9c67ac968713d2f016da52
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-12 12:58:31 -0500

    debugging with node

--- nodeJS/script.js
+++ nodeJS/script.js
@@ -1,7 +1,27 @@
 // function(exports, require, module, __filename, __dirname) {
-const message = require('./myscript')
-console.log(message)
+// const message = require('./myscript')
+// console.log(message) //Hi There!
 
 // }
 
-console.log(arguments)
+//========================
+// console.log(arguments)
+// console.log(require)
+// console.log(module)
+// console.log(__filename)
+// console.log(__dirname)
+
+const counterObject = require('./myscript')
+
+console.log(counterObject.getCounter())
+counterObject.incrementCounter()
+console.log(counterObject.getCounter())
+
+const newCounterObject = require('./myscript')
+
+console.log(newCounterObject.getCounter())
+counterObject.incrementCounter()
+
+const newestCounterObject = require('./myscript')
+
+console.log(newestCounterObject.getCounter())
